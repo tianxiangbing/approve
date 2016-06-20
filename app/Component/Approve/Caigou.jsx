520cc9c79466826bfeace1218e68c3c339a80133
田想兵 <tianxb@shinemo.com>
2016-06-20 18:26:14 +0800

    '采购明细'

--- app/Component/Approve/Caigou.jsx
+++ app/Component/Approve/Caigou.jsx
@@ -23,18 +23,35 @@ export default class Caigou extends Component{
 		newDetail.splice(index,1);
 		this.setState({detail:newDetail});
 	}
-	reRender(){
+	/*reRender(){
 		this.setState({detail:this.state.detail});
-	}
+	}*/
 	computeMoney(){
 		console.log(this.refs)
+		let sumMoney = 0;
+		this.state.detail.forEach((item,index)=>{
+			let money = this.refs['caigouDetail'+index].getMoney();
+			sumMoney+=money;
+		});
+		this.setState({sumPrice:sumMoney});
 	}
-
+	submit(){
+		let returnValue = false;
+		for (var i = 0 ,l= this.state.detail.length - 1; i <= l; i++) {
+			let item  = this.state.detail[i];
+			let validate = this.refs['caigouDetail'+i].validate();
+			if(validate.status == false){
+				alert(validate.text)
+				break;
+			}
+		};
+		return returnValue;
+	}
 	render(){
 		return (
 			<div>
 				<div className="txt-reason">
-					<textarea ref="applyResean" placeholder="请输入采购事由（必填）"/>
+					<textarea ref="applyResean"  maxLength ="60" placeholder="请输入采购事由（必填）"/>
 				</div>
 				<div className="row">
 					<span>期望交付日期</span>
@@ -44,13 +61,15 @@ export default class Caigou extends Component{
 					this.state.detail.map((item,index)=>{
 						let indx = index+1;
 						console.log("heloo:",item)
-						return <CaigouDetail ref="caigouDetail" index={indx} computeMoney={this.computeMoney.bind(this)} item={item} key={item.key} reRender={this.reRender.bind(this)} del={this.del.bind(this,index)}/>
+						return <CaigouDetail ref={"caigouDetail"+index} index={indx} computeMoney={this.computeMoney.bind(this)} item={item} key={item.key} del={this.del.bind(this,index)}/>
 					})
 				}
 				<div className="row add-row" onClick={this.AddRow.bind(this)}>
 					<i className="iconfont icon-113"/>添加采购明细
 				</div>
 				<div className="row sum-price">总价（元）:<span>{this.state.sumPrice}</span></div>
+
+				<a onClick={this.submit.bind(this)}>提交</a>
 			</div>
 			)
 	}
--- app/Component/Approve/CaigouDetail.jsx
+++ app/Component/Approve/CaigouDetail.jsx
@@ -4,14 +4,28 @@ let {Component} = React;
 export  default  class CaigouDetail extends Component{
 	constructor(props){
 		super(props);
-		this.state={item:{price:'',quantity:'',key:this.props.key}}
+		this.text = [{field:'item',text:'名称'},{field:'spec',text:'规格'},{field:'unit',text:'单位'},{field:'price',text:'价格'},{field:'quantity',text:'数量'}]
+		this.state={item:{price:'',quantity:'',spec:'',item:'',unit:''}}
 	}
 	del(index){
-		this.props.del(index);
-		this.props.reRender();
+		let hasValue= false;
+		for(var key in this.state.item){
+			if(this.state.item[key]!=""){
+				hasValue=true;
+			}
+		}
+		if(hasValue){
+			if(confirm("确定删除采购明细"+this.props.index+"?")){
+				this.props.del(this.props.index);
+			}
+		}else{
+			this.props.del(this.props.index);
+		}
+
+		//this.props.reRender();
 	}
 	componentDidMount(){
-		console.log("didMount")
+		//console.log("didMount")
 		this.setState({state:this.props.item});
 	}
 	change(field,e){
@@ -36,33 +50,43 @@ export  default  class CaigouDetail extends Component{
 			this.state.item[field]= value;
 			this.setState({item:this.state.item})
 		}
-		this.props.computeMoney();
+		this.props.computeMoney(this);
 	}
 	getMoney(){
 		let price = this.state.item.price||0;
 		let quantity = this.state.item.quantity||0;
 		return price*quantity;
 	}
+	validate(){
+		let returnValue = {status:true}
+		for (var i = this.text.length - 1; i >= 0; i--) {
+		 	let item = this.text[i];
+		 	if( this.state.item[item.field].length ==0){
+				returnValue = {status:false,text:"请输入采购明细"+this.props.index+"的"+item.text}
+			}
+		};
+		return returnValue;
+	}
 	render(){
 		return (
 			<div>
 				<div className="detail" title={this.props.title}>
-					<h3>采购明细({this.props.index}) <a onClick={this.props.del} className={this.props.index>1?"del":"hide"}>删除</a></h3>
+					<h3>采购明细({this.props.index}) <a onClick={this.del.bind(this)} className={this.props.index>1?"del":"hide"}>删除</a></h3>
 					<div className="formbox">
 						<div className="rowinput">
-							名称<input type="text" value={this.state.item.item} onChange={this.change.bind(this,"item")} placeholder="请输入（必填）"/>
+							名称<input type="text" ref="item" maxLength ="60" value={this.state.item.item} onChange={this.change.bind(this,"item")} placeholder="请输入（必填）"/>
 						</div>
 						<div className="rowinput">
-							规格<input type="text" value={this.state.item.spec} onChange={this.change.bind(this,"spec")} placeholder="请输入（必填）"/>
+							规格<input type="text" ref="spec" maxLength ="60" value={this.state.item.spec} onChange={this.change.bind(this,"spec")} placeholder="请输入（必填）"/>
 						</div>
 						<div className="rowinput">
-							单位<input type="text" value={this.state.item.unit} onChange={this.change.bind(this,"unit")} placeholder="请输入（必填）"/>
+							单位<input type="text" ref="unit" maxLength ="60" value={this.state.item.unit} onChange={this.change.bind(this,"unit")} placeholder="请输入（必填）"/>
 						</div>
 						<div className="rowinput">
-							单价（元）<input type="text" value={this.state.item.price} onChange={this.change.bind(this,"price")} placeholder="请输入（必填）"/>
+							单价（元）<input type="text" ref="price" maxLength ="10" value={this.state.item.price} onChange={this.change.bind(this,"price")} placeholder="请输入（必填）"/>
 						</div>
 						<div className="rowinput">
-							数量<input type="text" value={this.state.item.quantity} onChange={this.change.bind(this,"quantity")} placeholder="请输入（必填）"/>
+							数量<input type="text" ref="quantity" maxLength ="9" value={this.state.item.quantity} onChange={this.change.bind(this,"quantity")} placeholder="请输入（必填）"/>
 						</div>
 					</div>
 				</div>
